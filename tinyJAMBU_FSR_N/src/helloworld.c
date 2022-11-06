e3fd7fb3ed5559b229bc97490abdf47735453650
russelljh <85257210+russelljh@users.noreply.github.com>
2022-11-05 23:19:44 -0700

    adds tinyJAMBU to main c file

--- tinyJAMBU_FSR_N/src/helloworld.c
+++ tinyJAMBU_FSR_N/src/helloworld.c
@@ -50,13 +50,238 @@
 #include "xil_printf.h"
 #include "tinyJAMBU_FSR_128_N.h"
 
+#define NROUND1 5
+#define NROUND2 8
+
+#define FrameBitsIV  0x10
+#define FrameBitsAD  0x30
+#define FrameBitsPC  0x50  //Framebits for plaintext/ciphertext
+#define FrameBitsFinalization 0x70
+
+void state_update(unsigned int* state, const unsigned char* key, unsigned int steps) {
+
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG0_OFFSET, state[0]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG1_OFFSET, state[1]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG2_OFFSET, state[2]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG3_OFFSET, state[3]);
+
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG4_OFFSET, ((unsigned int*)key)[0]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG5_OFFSET, ((unsigned int*)key)[1]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG6_OFFSET, ((unsigned int*)key)[2]);
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG7_OFFSET, ((unsigned int*)key)[3]);
+
+	TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG8_OFFSET, steps);
+
+	while (!(TINYJAMBU_FSR_128_N_mReadReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG9_OFFSET) && 0x00000001)) {
+
+	}
+	state[0] = TINYJAMBU_FSR_128_N_mReadReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG9_OFFSET);
+	state[1] = TINYJAMBU_FSR_128_N_mReadReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG10_OFFSET);
+	state[2] = TINYJAMBU_FSR_128_N_mReadReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG11_OFFSET);
+	state[3] = TINYJAMBU_FSR_128_N_mReadReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG12_OFFSET);
+}
+
+
+// The initialization
+/* The input to initialization is the 128-bit key; 96-bit IV;*/
+void initialization(const unsigned char *key, const unsigned char *iv, unsigned int *state)
+{
+        int i;
+
+        //initialize the state as 0
+        for (i = 0; i < 4; i++) state[i] = 0;
+
+        //update the state with the key
+        state_update(state, key, NROUND2);
+
+        //introduce IV into the state
+        for (i = 0;  i < 3; i++)
+        {
+                state[1] ^= FrameBitsIV;
+                state_update(state, key, NROUND1);
+                state[3] ^= ((unsigned int*)iv)[i];
+        }
+}
+
+//process the associated data
+void process_ad(const unsigned char *k, const unsigned char *ad, unsigned long long adlen, unsigned int *state)
+{
+        unsigned long long i;
+        unsigned int j;
+
+        for (i = 0; i < (adlen >> 2); i++)
+        {
+                state[1] ^= FrameBitsAD;
+                state_update(state, k, NROUND1);
+                state[3] ^= ((unsigned int*)ad)[i];
+        }
+
+        // if adlen is not a multiple of 4, we process the remaining bytes
+        if ((adlen & 3) > 0)
+        {
+                state[1] ^= FrameBitsAD;
+                state_update(state, k, NROUND1);
+                for (j = 0; j < (adlen & 3); j++)  ((unsigned char*)state)[12 + j] ^= ad[(i << 2) + j];
+                state[1] ^= adlen & 3;
+        }
+}
+
+//encrypt plaintext
+int crypto_aead_encrypt(
+	unsigned char *c, unsigned long long *clen,
+	const unsigned char *m, unsigned long long mlen,
+	const unsigned char *ad, unsigned long long adlen,
+	const unsigned char *nsec,
+	const unsigned char *npub,
+	const unsigned char *k
+)
+{
+        unsigned long long i;
+        unsigned int j;
+        unsigned char mac[8];
+        unsigned int state[4];
+
+        //initialization stage
+        initialization(k, npub, state);
+
+        //process the associated data
+        process_ad(k, ad, adlen, state);
+
+        //process the plaintext
+        for (i = 0; i < (mlen >> 2); i++)
+        {
+                state[1] ^= FrameBitsPC;
+                state_update(state, k, NROUND2);
+                state[3] ^= ((unsigned int*)m)[i];
+                ((unsigned int*)c)[i] = state[2] ^ ((unsigned int*)m)[i];
+        }
+        // if mlen is not a multiple of 4, we process the remaining bytes
+        if ((mlen & 3) > 0)
+        {
+                state[1] ^= FrameBitsPC;
+                state_update(state, k, NROUND2);
+                for (j = 0; j < (mlen & 3); j++)
+                {
+                        ((unsigned char*)state)[12 + j] ^= m[(i << 2) + j];
+                        c[(i << 2) + j] = ((unsigned char*)state)[8 + j] ^ m[(i << 2) + j];
+                }
+                state[1] ^= mlen & 3;
+        }
+
+        //finalization stage, we assume that the tag length is 8 bytes
+        state[1] ^= FrameBitsFinalization;
+        state_update(state, k, NROUND2);
+        ((unsigned int*)mac)[0] = state[2];
+
+        state[1] ^= FrameBitsFinalization;
+        state_update(state, k, NROUND1);
+        ((unsigned int*)mac)[1] = state[2];
+
+        *clen = mlen + 8;
+        for (j = 0; j < 8; j++) c[mlen+j] = mac[j];
+
+        return 0;
+}
+
+//decrypt a message
+int crypto_aead_decrypt(
+	unsigned char *m, unsigned long long *mlen,
+	unsigned char *nsec,
+	const unsigned char *c, unsigned long long clen,
+	const unsigned char *ad, unsigned long long adlen,
+	const unsigned char *npub,
+	const unsigned char *k
+)
+{
+        unsigned long long i;
+        unsigned int j, check = 0;
+        unsigned char mac[8];
+        unsigned int state[4];
+
+        *mlen = clen - 8;
+
+        //initialization stage
+        initialization(k, npub, state);
+
+        //process the associated data
+        process_ad(k, ad, adlen, state);
+
+        //process the ciphertext
+        for (i = 0; i < (*mlen >> 2); i++)
+        {
+                state[1] ^= FrameBitsPC;
+                state_update(state, k, NROUND2);
+                ((unsigned int*)m)[i] = state[2] ^ ((unsigned int*)c)[i];
+                state[3] ^= ((unsigned int*)m)[i];
+        }
+        // if mlen is not a multiple of 4, we process the remaining bytes
+        if ((*mlen & 3) > 0)
+        {
+                state[1] ^= FrameBitsPC;
+                state_update(state, k, NROUND2);
+                for (j = 0; j < (*mlen & 3); j++)
+                {
+                        m[(i << 2) + j] = c[(i << 2) + j] ^ ((unsigned char*)state)[8 + j];
+                        ((unsigned char*)state)[12 + j] ^= m[(i << 2) + j];
+                }
+                state[1] ^= *mlen & 3;
+        }
+
+        //finalization stage, we assume that the tag length is 8 bytes
+        state[1] ^= FrameBitsFinalization;
+        state_update(state, k, NROUND2);
+        ((unsigned int*)mac)[0] = state[2];
+
+        state[1] ^= FrameBitsFinalization;
+        state_update(state, k, NROUND1);
+        ((unsigned int*)mac)[1] = state[2];
+
+        //verification of the authentication tag
+        for (j = 0; j < 8; j++) { check |= (mac[j] ^ c[clen - 8 + j]); }
+        if (check == 0) return 0;
+        else return -1;
+}
+
+void test_encrypt_decrypt() {
+//    unsigned int state[4] = {0, 0, 0, 0};
+    const unsigned char key[16] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F};
+    const unsigned char npub[12] = {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B};
+    unsigned char c[80];
+    unsigned long long clen = 0;
+    unsigned char m[80] = {"abcd"};
+    unsigned long long mlen = 4;
+    unsigned char ad[80];
+    unsigned char adlen = 0;
+    unsigned char nsec[80] = {};
+
+    printf("Running encrypt\n");
+    crypto_aead_encrypt(
+        c, &clen,
+        m, mlen,
+        ad, adlen,
+        nsec,
+        npub,
+        key);
+    printf("clen is: %d \n", (unsigned int)clen);
+    for (size_t i = 0; i < clen; i++)
+    {
+        printf("%02X \n", c[i]);
+    }
+    printf("Running decrypt\n");
+    if (crypto_aead_decrypt(m, &mlen, nsec, c, clen, ad, adlen, npub, key) == 0)
+    {
+        printf("tag is verified\n");
+    } else {
+        printf("tag is not verified\n");
+    }
+}
 int main()
 {
     init_platform();
 
     print("Hello World\n\r");
     print("Successfully ran Hello World application");
-    TINYJAMBU_FSR_128_N_mWriteReg(0x44a00000, TINYJAMBU_FSR_128_N_S00_AXI_SLV_REG0_OFFSET, 0);
+    void test_encrypt_decrypt();
     cleanup_platform();
     return 0;
 }
